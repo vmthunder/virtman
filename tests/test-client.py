ccc8995429c33c6def94c1cbfececbcb1f52f9ad
licyh <licyh@126.com>
2014-03-13 19:33:33 +0800

    add wsgi

---
+++ tests/test-client.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import requests
+#import json
+
+
+class Client(object):
+
+    USER_AGENT = 'python-vmthunderclient'
+
+    def __init__(self, name='vmthunder'):
+        self.name = name
+
+    def requests(self, url, method, **kwargs):
+        resp = requests.request(method, url, **kwargs)
+        #body = json.loads(resp.text) 
+        body = 'Is Body'
+        return resp, body
+             
+    def get(self, url, **kwargs):
+        return self.requests(url, 'GET', **kwargs) 
+    
+    def post(self, url, **kwargs):
+        return self.requests(url, 'POST', **kwargs)
+    
+    def put(self, url, **kwargs):
+        return self.requests(url, 'PUT', **kwargs)
+    
+    def delete(self, url, **kwargs):
+        return self.requests(url, 'DELETE', **kwargs)
+
+def test1():
+    c = Client()
+    url = 'http://10.107.14.50:8041'
+    print c.get(url)
+    print c.post(url)
+    print c.put(url)
+    print c.delete(url)
+
+def test2():
+    url1 = 'http://127.0.0.1:8080'
+    url2 = 'http://127.0.0.1:8080/show'
+    url3 = 'http://127.0.0.1:8080/calc?operator=plus&operand1=12&operand2=23'
+    print requests.get(url1)
+    print requests.get(url2)
+    print requests.get(url3)
+
+if __name__ == '__main__' :
+    #test1()
+    test2()
--- vmthunder/api.py
+++ vmthunder/api.py
@@ -1,6 +1,74 @@
 #!/usr/bin/env python
 
+import os
+import webob
+from webob import Request
+from webob import Response
 
+class Hello():
+    def __init__(self):
+        pass
+    def __call__(self, environ, start_response):
+        status = '200 OK'
+        response_headers = [('Content-type','text/plain')]
+        start_response(status, response_headers)
+        return ['Hello World!\n']
+    @classmethod
+    def factory(cls, global_conf, **kwargs):
+        print 'in Hello.factory', global_conf, kwargs
+        return Hello()
+
+class ShowVersion():
+    def __init__(self):
+        pass
+    def __call__(self, environ, start_response):
+        start_response("200 OK",[("Content-type", "text/plain")])  
+        return ["Paste Deploy LAB: Version = 1.0.0",]  
+    @classmethod  
+    def factory(cls, global_conf, **kwargs):  
+        print "in ShowVersion.factory", global_conf, kwargs  
+        return ShowVersion()
+ 
+class Calculator():  
+    def __init__(self):  
+        pass  
+    def __call__(self, environ, start_response):  
+        req = Request(environ)
+        res = Response()
+        # get operands  
+        operator = req.GET.get("operator", None)  
+        operand1 = req.GET.get("operand1", None)  
+        operand2 = req.GET.get("operand2", None)  
+        print req.GET
+        opnd1 = int(operand1)  
+        opnd2 = int(operand2)  
+        if operator == u'plus':  
+            opnd1 = opnd1 + opnd2  
+        elif operator == u'minus':  
+            opnd1 = opnd1 - opnd2  
+        elif operator == u'star':  
+            opnd1 = opnd1 * opnd2  
+        elif operator == u'slash':  
+            opnd1 = opnd1 / opnd2  
+        res.status = "200 OK"  
+        res.content_type = "text/plain"  
+        res.body = "%s /nRESULT= %d" % (str(req.GET) , opnd1)  
+        return res(environ, start_response)  
+    @classmethod
+    def factory(cls, global_conf, **kwargs):  
+        print "in Calculator.factory", global_conf, kwargs  
+        return Calculator()  
+
+if __name__ == '__main__':
+    from paste.deploy import loadapp
+    from wsgiref.simple_server import make_server
+    config_file = 'api-paste.ini'
+    app_name = 'all'
+    wsgi_app = loadapp('config:%s' %os.path.abspath(config_file), app_name)
+    server = make_server('localhost', 8080, wsgi_app)
+    server.serve_forever()
+
+'''
 def hello_wsgi(environ, start_response):
     """Simple WSGI application"""
     status = '200 OK'
@@ -12,3 +80,4 @@ def app_factory(global_config, **local_config):
     """This function wraps our simple WSGI app so it
     can be used with paste.deploy"""
     return hello_wsgi
+'''

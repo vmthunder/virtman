8b118ef74b8fcb51329382b4d4b45e1909129054
ChenAlong <562963835@qq.com>
2014-03-20 00:15:54 +0800

    update the file cn-test.py

--- tests/cn-test.py
+++ tests/cn-test.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 import sys
 sys.path.append("..")
+import os
 from vmthunder.computenode import ComputeNode
 from testsuit import try_exec
 
 if __name__ == '__main__':
     prop1 = {}
-    prop1['target_portal'] = '192.168.122.202:3260'
-    prop1['target_iqn'] = 'iqn.2010-10.org.openstack:3'
+    prop1['target_portal'] = '10.107.14.162:3260'
+    prop1['target_iqn'] = 'iqn.2010-10.org.openstack:4'
     prop1['target_lun'] = 1
     prop2 = {}
-    prop2['target_portal'] = '192.168.122.203:3260'
-    prop2['target_iqn'] = 'iqn.2010-10.org.openstack:3'
+    prop2['target_portal'] = '10.107.14.163:3260'
+    prop2['target_iqn'] = 'iqn.2010-10.org.openstack:4'
     prop2['target_lun'] = 1
 
     cn = ComputeNode()
-    #cn.fake_code('3', 'vm1', [prop1,prop2], '/dev/loop1')
-    multipath_name = cn._multipath_name('3')
-    out = cn.dm.get_table(multipath_name)
-    print out
-    cn.destroy('3', 'vm1', [prop1,prop2], '/dev/loop1')
+    cn.start_vm('4', 'vm1', [prop1], '/dev/loop1')
+    cn.start_vm('4', 'vm2', [prop2], '/dev/loop2')
+    cn.adjust_structure('4', [prop1, prop2], '')
+    cn.delete_vm('vm1', [prop1,prop2])
+    cn.delete_vm('vm2', [prop1,prop2])

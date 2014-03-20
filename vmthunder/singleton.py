e27e63e20aadee56dbf2325b06507d85d85592a1
Ziyang Li <lzynudt@gmail.com>
2014-03-20 22:20:04 +0800

    add get_instance

--- vmthunder/singleton.py
+++ vmthunder/singleton.py
@@ -1,3 +1,10 @@
+
+def get_instance(cls, *args, **kv):
+    if cls in cls.objs:
+        return cls.objs[cls]
+    else:
+        return cls(*args, **kv)
+
 class SingleTon(object):
     objs  = {}
     def __new__(cls, *args, **kv):

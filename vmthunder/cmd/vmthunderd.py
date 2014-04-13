0c5af0f25fe52110de51f468eadd45e7622b84f3
Ziyang Li <lzynudt@gmail.com>
2014-04-14 01:52:06 +0800

    ddadeewaf

--- vmthunder/cmd/vmthunderd.py
+++ vmthunder/cmd/vmthunderd.py
@@ -28,7 +28,7 @@ def start():
 
 if __name__ == '__main__':
     CONF(sys.argv[1:], project='vmthunder',
-         default_config_files = ['../etc/vmthunder.conf'])
+         default_config_files = ['/root/develop/VMThunder/etc/vmthunder/api-paste.ini'])
     #print CONF.fcg_name, CONF.fcg_ssds, CONF.fcg_blocksize, CONF.fcg_pattern
     logging.setup('vmthunder')
     start()

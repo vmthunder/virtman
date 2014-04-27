0ebe2bc21274420a9a03757048ac98e8b77aabe0
Ziyang Li <lzynudt@gmail.com>
2014-04-27 22:28:38 +0800

    change lock

--- vmthunder/session.py
+++ vmthunder/session.py
@@ -46,7 +46,7 @@ class Session(object):
         self.peer_id = ''
         self.target_id = 0
         self.__status = STATUS.empty
-        self.status_lock = threading.RLock()
+        self.status_lock = threading.Lock()
         LOG.debug("VMThunder: create a session of volume_name %s" % self.volume_name)
 
     @property
